a173589b797dc5934990d100ec3fb6728b5a2866
Forrest M <forrestm@vt.edu>
2023-02-20 00:42:59 -0500

    question mark

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	base: "/pyInvenio.github.io/",
+/** @type {import('vite').UserConfig} */
+const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
-});
+};
+
+export default config;
